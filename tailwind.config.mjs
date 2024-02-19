276fc135100c4c5b31c5d4e9a834cabda50f634f
angelcgar <146035436+angelcgar@users.noreply.github.com>
2024-02-19 10:14:54 -0600

    change UA

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+		extend: {
+  	"colors": {
+    	"ancg": {
+      	50: '#f6f7f9',
+    		100: '#eceef2',
+    		200: '#d4d9e3',
+    		300: '#aeb9cb',
+    		400: '#8292ae',
+    		500: '#627595',
+    		600: '#4e5e7b',
+    		700: '#404c64',
+    		800: '#384154',
+    		900: '#323948',
+    		950: '#1f232d',
+    	}
+  	}
+	}
+},
 	plugins: [],
 }
