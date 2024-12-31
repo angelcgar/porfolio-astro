d8039f293cb3c1ace0163945a81b9a792840098d
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-12-31 15:21:21 -0600

    refactor: actualizar estructura de datos de proyectos y mejorar visualización

--- src/interfaces/experience.interface.ts
+++ src/interfaces/experience.interface.ts
@@ -4,3 +4,19 @@ export interface experience {
   description: string;
   link: string;
 }
+
+export interface Proyectos {
+  name: string;
+  title: string;
+  body: Body;
+  href: string;
+  imageHref: string;
+  github: string;
+  tags: string[];
+  id: number;
+}
+
+interface Body {
+  pequeño: string;
+  largo: string;
+}
